b723c2b6ef995a64da889932187b060bfe387c27
shiboqing <shiboqing@fenbi.com>
2021-05-17 12:16:10 +0800

    ADD: treeNode struct & buildTree

--- structs/treeNode.go
+++ structs/treeNode.go
@@ -1,10 +1,29 @@
 package structs
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func buildTree(nums []int) *TreeNode {
+    if len(nums) == 0 {
+        return new(TreeNode)
+    }
+    treeNodeArray := make([]*TreeNode, 0)
+    for i := 0; i < len(nums); i++ {
+        tempNode := new(TreeNode)
+        tempNode.Val = nums[i]
+        treeNodeArray = append(treeNodeArray, tempNode)
+    }
+    for i := 0; i < len(nums) / 2; i++ {
+        leftIndex, rightIndex := i * 2 + 1, i * 2 + 2
+        if leftIndex < len(nums) {
+            treeNodeArray[i].Left = treeNodeArray[leftIndex]
+        }
+        if rightIndex < len(nums) {
+            treeNodeArray[i].Right = treeNodeArray[rightIndex]
+        }
+    }
+    return treeNodeArray[0]
+}
