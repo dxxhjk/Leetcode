277a8e5d3b082167a7f08407e26ff75fd9e1ac78
史伯卿 <shiboqing@shiboqingdeMacBook-Pro.local>
2021-11-10 17:16:43 +0800

    11月10日每日一题

--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
-	str := "asd"
-	fmt.Println(reflect.TypeOf(str), reflect.ValueOf(str))
-	for _, b := range str {
-		fmt.Println(reflect.TypeOf(b), reflect.ValueOf(b), b, byte(b))
+	str := [3]int{1,2,3}
+	fmt.Println(&str[0])
+	for _, v := range str {
+		fmt.Println(&v, v)
 	}
-	fmt.Println(reflect.TypeOf(str[0]), reflect.ValueOf(str[0]))
-	m := make(map[int]int)
-	m[1] = 10
-	fmt.Println(m)
-	test(m)
-	fmt.Println(m)
-}
-
-func test(m map[int]int) {
-	m[1] -= 2
 }
